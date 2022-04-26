84cfd60873d655acc93cfb94d81e17caa0a50616
Vladimir <yyaakkovlev@gmail.com>
2022-04-27 01:20:57 +0300

    make it better

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from main import main
+from main import *
 
 if __name__ == "__main__":
-    main()
+    db_session.global_init("db/database.db")
+    app.register_blueprint(authorization.blueprint)
+    app.register_blueprint(home.blueprint)
+    api.add_resource(home_api.TokensListResource, '/api/tokens')
+    api.add_resource(home_api.UserTokenListResource, '/api/users/tokens')
+    api.add_resource(home_api.UserTokenResource, '/api/user/token')
+    app.run(port=os.getenv('PORT', 8080), host='0.0.0.0')
