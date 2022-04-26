65f94d67bbb07f3594036557d1158da69aa64f27
Vladimir <yyaakkovlev@gmail.com>
2022-04-27 01:30:54 +0300

    make it better

--- main.py
+++ main.py
@@ -60,6 +60,14 @@ def block_wallet():
     return resp
 
 
+db_session.global_init("db/database.db")
+app.register_blueprint(authorization.blueprint)
+app.register_blueprint(home.blueprint)
+api.add_resource(home_api.TokensListResource, '/api/tokens')
+api.add_resource(home_api.UserTokenListResource, '/api/users/tokens')
+api.add_resource(home_api.UserTokenResource, '/api/user/token')
+
+
 def main():
     db_session.global_init("db/database.db")
     app.register_blueprint(authorization.blueprint)
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
 from main import *
 
 if __name__ == "__main__":
-    db_session.global_init("db/database.db")
-    app.register_blueprint(authorization.blueprint)
-    app.register_blueprint(home.blueprint)
-    api.add_resource(home_api.TokensListResource, '/api/tokens')
-    api.add_resource(home_api.UserTokenListResource, '/api/users/tokens')
-    api.add_resource(home_api.UserTokenResource, '/api/user/token')
     app.run(port=os.getenv('PORT', 8080), host='0.0.0.0')
